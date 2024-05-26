2b72cd3fbf0fd6b4cd09d247cedd8dc81c9e5a86
Szymon Wieloch <szymon.wieloch@gmail.com>
2024-05-27 01:04:02 +0200

    Added double, partial split support

--- blackjack_ai/blackjack/game.go
+++ blackjack_ai/blackjack/game.go
@@ -11,66 +11,106 @@ func Play(player Player, options ...GameOption) Cents {
 		opt(&opts)
 	}
 	var result Cents
-	var deck cards.Deck = cards.NewDeck(cards.WithDecks(opts.decks))
+	var deck cards.Deck
 	for range opts.games {
-		// TODO reset deck
+		maybeResetDeck(&deck, opts.decks)
 		result += playSingle(&deck, player, opts)
 	}
 	return result
 }
 
-func playSingle(deck *cards.Deck, player Player, opts gameOptions) Cents {
-	bet := player.Bet()
+func maybeResetDeck(deck *cards.Deck, deckCfg uint) {
+	if len(*deck) < (52 * int(deckCfg) / 3) {
+		*deck = cards.NewDeck(cards.WithDecks(deckCfg))
+	}
+}
 
-	playerHand := Hand{deck.Pop(), deck.Pop()}
+type handState struct {
+	hand Hand
+	bet  Cents
+}
+
+func playSingle(deck *cards.Deck, player Player, opts gameOptions) Cents {
+	hs := handState{
+		bet:  player.Bet(),
+		hand: Hand{deck.Pop(), deck.Pop()},
+	}
+
 	dealerHand := Hand{deck.Pop(), deck.Pop()}
 
-	player.OnStart(playerHand, dealerHand[:1])
+	player.OnStart(hs.hand, dealerHand[:1])
 
-	if isBlackjack(playerHand) && isBlackjack(dealerHand) {
-		player.OnGameCompleted(playerHand, dealerHand, 0)
+	if isBlackjack(hs.hand) && isBlackjack(dealerHand) {
+		player.OnGameCompleted(hs.hand, dealerHand, 0)
 		return 0
 	}
 	if isBlackjack(dealerHand) {
-		player.OnGameCompleted(playerHand, dealerHand, -bet)
-		return -bet
+		player.OnGameCompleted(hs.hand, dealerHand, -hs.bet)
+		return -hs.bet
 	}
-	if isBlackjack(playerHand) {
-		result := Cents(float64(bet) * opts.blackjackPayout)
-		player.OnGameCompleted(playerHand, dealerHand, result)
+	if isBlackjack(hs.hand) {
+		result := Cents(float64(hs.bet) * opts.blackjackPayout)
+		player.OnGameCompleted(hs.hand, dealerHand, result)
 		return result
 	}
-loop:
-	for {
-		switch player.MakeDecision(playerHand, dealerHand[:1]) {
-		case Stand:
-			break loop
-		case Hit:
-			playerHand = append(playerHand, deck.Pop())
-		}
-		if isBusted(playerHand) {
-			player.OnGameCompleted(playerHand, dealerHand, -bet)
-			return -bet
-		}
+	playerTurn(&hs, player, dealerHand, deck)
+	if isBusted(hs.hand) {
+		player.OnGameCompleted(hs.hand, dealerHand, -hs.bet)
+		return -hs.bet
 	}
 
 	for dealerNeedsToBid(dealerHand) {
 		dealerHand = append(dealerHand, deck.Pop())
 	}
+	return checkResult(hs, dealerHand, player)
+}
+
+func playerTurn(hs *handState, player Player, dealerHand Hand, deck *cards.Deck) []handState {
+	// return on stand or busted
+	newHands := []handState{}
+	for {
+		switch player.MakeDecision(hs.hand, dealerHand[:1]) {
+		case Stand:
+			return newHands
+		case Hit:
+			hs.hand = append(hs.hand, deck.Pop())
+		case Double:
+			hs.bet *= 2
+			hs.hand = append(hs.hand, deck.Pop())
+			return newHands
+		case Split:
+			if !CanSplit(hs.hand) {
+				panic("Invalid user move: split")
+			}
+			hs.hand = hs.hand[:1]
+			newHands = append(newHands, handState{
+				hand: hs.hand[1:],
+				bet:  player.Bet(),
+			})
+			// newHand :=
+		}
+		if isBusted(hs.hand) {
+			return newHands
+		}
+	}
+}
+
+// After the dealer collected all cards, performs check and side-effects on each hand state
+func checkResult(hs handState, dealerHand Hand, player Player) Cents {
 	if isBusted(dealerHand) {
-		player.OnGameCompleted(playerHand, dealerHand, bet)
-		return bet
+		player.OnGameCompleted(hs.hand, dealerHand, hs.bet)
+		return hs.bet
 	}
 	dealerScore, _ := Score(dealerHand)
-	playerScore, _ := Score(playerHand)
+	playerScore, _ := Score(hs.hand)
 	if dealerScore > playerScore {
-		player.OnGameCompleted(playerHand, dealerHand, -bet)
-		return -bet
+		player.OnGameCompleted(hs.hand, dealerHand, -hs.bet)
+		return -hs.bet
 	} else if dealerScore < playerScore {
-		player.OnGameCompleted(playerHand, dealerHand, bet)
-		return bet
+		player.OnGameCompleted(hs.hand, dealerHand, hs.bet)
+		return hs.bet
 	} else {
-		player.OnGameCompleted(playerHand, dealerHand, 0)
+		player.OnGameCompleted(hs.hand, dealerHand, 0)
 		return 0
 	}
 }
