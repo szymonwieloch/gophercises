35b2b8474add49dae56d8baea95c8cb79631b8a7
Szymon Wieloch <szymon.wieloch@gmail.com>
2024-06-04 13:49:10 +0200

    Fully asynchronous read of the API

--- hackernews/client/client.go
+++ hackernews/client/client.go
@@ -43,14 +43,14 @@ func GetStory(id StoryID) (Story, error) {
 }
 
 type Story struct {
-	By          string `json:"by"`
-	Descendants int    `json:"descendants"`
-	ID          int    `json:"id"`
-	Kids        []int  `json:"kids"`
-	Score       int    `json:"score"`
-	Time        int    `json:"time"`
-	Title       string `json:"title"`
-	Type        string `json:"type"`
+	By          string  `json:"by"`
+	Descendants int     `json:"descendants"`
+	ID          StoryID `json:"id"`
+	Kids        []int   `json:"kids"`
+	Score       int     `json:"score"`
+	Time        int     `json:"time"`
+	Title       string  `json:"title"`
+	Type        string  `json:"type"`
 
 	// Only one of these should exist
 	Text string `json:"text"`
--- hackernews/main.go
+++ hackernews/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	_ "embed"
-	"fmt"
 	"log"
 	"net/http"
+	"net/url"
+	"slices"
+	"strings"
 	"text/template"
 	"time"
 
@@ -19,30 +21,28 @@ func init() {
 	templates = template.Must(template.New("main").Parse(templateStr))
 }
 
+const wantStories = 30
+
 func handler(w http.ResponseWriter, r *http.Request) {
-	topStories, err := client.TopStories()
+	start := time.Now()
+	stories, err := getTopStories(wantStories)
 	if err != nil {
 		http.Error(w, "Failed to load top stories", http.StatusInternalServerError)
 		return
 	}
-	detailedStories := topStories[:30]
-	var stories []client.Story
-	for _, ds := range detailedStories {
-		story, err := client.GetStory(ds)
-		if err != nil {
-			http.Error(w, "Failed to load top stories", http.StatusInternalServerError)
-			return
-		}
-		stories = append(stories, story)
-	}
 
 	var items []item
 	for _, story := range stories {
-		items = append(items, item{Story: story, Host: "google.com"})
+		var host string
+		u, err := url.Parse(story.URL)
+		if err == nil {
+			host = strings.TrimPrefix(u.Hostname(), "www.")
+		}
+		items = append(items, item{Story: story, Host: host})
 	}
 	td := templateData{
 		Stories: items,
-		Time:    time.Second * 5,
+		Time:    time.Since(start),
 	}
 	err = templates.Execute(w, &td)
 	if err != nil {
@@ -51,13 +51,76 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func main() {
-	stories, err := client.TopStories()
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(stories)
+func takeN[T any](in []T, n int) ([]T, []T) {
+	take := min(n, len(in))
+	return in[:take], in[take:]
+}
 
+type asyncResult struct {
+	story client.Story
+	err   error
+}
+
+func getBatch(storyIDs []client.StoryID) []client.Story {
+	ch := make(chan asyncResult)
+	for _, storyId := range storyIDs {
+		go func(storyId client.StoryID) {
+			story, err := client.GetStory(storyId)
+			ch <- asyncResult{
+				story: story,
+				err:   err,
+			}
+		}(storyId)
+	}
+	var stories []client.Story
+	for range storyIDs {
+		ar := <-ch
+		if ar.err != nil {
+			log.Println("Error getting story: ", ar.err)
+			continue
+		}
+		stories = append(stories, ar.story)
+	}
+	return stories
+}
+
+func getTopStories(count int) ([]client.Story, error) {
+	topStories, err := client.TopStories()
+	if err != nil {
+		return nil, err
+	}
+	remaining := topStories
+	var stories []client.Story
+	for len(stories) < count && len(remaining) > 0 {
+		missing := count - len(stories)
+		batchSize := (missing * 5 / 4) + 3
+		var batchIDs []client.StoryID
+		batchIDs, remaining = takeN(remaining, batchSize)
+		batchStories := getBatch(batchIDs)
+		for _, story := range batchStories {
+			if isStoryLink(story) {
+				stories = append(stories, story)
+			}
+		}
+	}
+	// we may have too many stories
+	stories = stories[:min(count, len(stories))]
+	//sort
+	storyIDToIdx := map[client.StoryID]int{}
+	for idx, storyId := range topStories {
+		storyIDToIdx[storyId] = idx
+	}
+	slices.SortFunc(stories, func(a, b client.Story) int {
+		return storyIDToIdx[a.ID] - storyIDToIdx[b.ID]
+	})
+	return stories, nil
+}
+
+func isStoryLink(story client.Story) bool {
+	return story.Type == "story" && story.URL != ""
+}
+
+func main() {
 	http.HandleFunc("/", handler)
 
 	// Start the server
